b18f7cd6284d7dc2532c2f74b5f3da2333d1f9cc
Naeem Sajjad <naeem9924066@gmail.com>
2026-02-21 22:19:27 +0500

    solved valid prenthesis problem no 20

---
+++ 20_Valid_Parenthesis.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack=[]
+    let pairs={
+        ")":"(",
+        "}":"{",
+        "]":"["
+    }
+    for(let char of s){
+        if(char==="(" || char==="{" || char==="["){
+            stack.push(char)
+        }else{
+            if(stack.pop()!==pairs[char]){
+                return false
+            }
+        }
+    }
+    return stack.length===0;
+};
+
+console.log(isValid("()")) // Output: true
+console.log(isValid("()[]{}")) // Output: true
+console.log(isValid("(]")) // Output: false
