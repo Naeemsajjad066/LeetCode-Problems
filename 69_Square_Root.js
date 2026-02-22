80ebd5afba009eb8faf34ab7abb5b7cfd8d62db4
Naeem Sajjad <naeem9924066@gmail.com>
2026-02-22 23:28:46 +0500

    solved square root problem 69

---
+++ 69_Square_Root.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if(x<2){
+        return x
+    }
+
+    let left=1
+    let right=Math.floor(x/2);
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        let square=mid*mid;
+        if(square==x){
+            return mid;
+        }
+        if(square<x){
+            left=mid+1
+        }
+        if(square>x){
+            right =mid-1
+        }
+    }
+    return right
+};
+console.log(mySqrt(4)) // Output: 2
+console.log(mySqrt(8)) // Output: 2
