6064476956cebcf10e6503ce7b5d41b4b95d52f1
Naeem Sajjad <naeem9924066@gmail.com>
2026-03-04 18:00:55 +0500

    solved question 169 majority element by two vairables candidate and count

---
+++ 169_Majority_Element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count=0;
+    let candidate=null;
+
+    for(let num of nums){
+        if(count==0){
+            candidate=num;
+        }
+        if(num===candidate){
+            count++;
+        }else{
+            count--
+        }
+    }
+    return candidate;
+};
+console.log(majorityElement([3,2,3]))//3    
+console.log(majorityElement([2,2,1,1,1,2,2]))//2
